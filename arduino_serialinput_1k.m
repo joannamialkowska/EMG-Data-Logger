edbb9cbb638a0d31b8feca514677dd677da04d5f
Joanna Mialkowska <124645771+joannamialkowska@users.noreply.github.com>
2024-03-12 11:47:20 -0700

    arduino_serialinput_1k.m

--- arduino_serialinput_1k.m
+++ arduino_serialinput_1k.m
@@ -3,16 +3,9 @@ function [data] = arduino_serialinput_1k(samplingRate,numSec,baudRate,com)
 %Arduino port setup
 device = serialport(com,baudRate);    
 configureTerminator(device,'CR/LF')
-                     
-% data = uint32.empty(2,0);
-% formatSpec = '%d %d';
 
-%v = uint32.empty(1,0);
 v = [];
 formatSpec = '%d';
-
-% t = uint32.empty(1,0);
-% sampletime = 0;
 
 count = 0;
 while count < numSec*samplingRate
@@ -20,14 +13,12 @@ while count < numSec*samplingRate
     line = readline(device);
     sample = sscanf(line,formatSpec);
     v = [v;sample];
-    % t = [t;sampletime];
-    % sampletime = sampletime + 1;
-    
-    %data = [data; sample];
 end
 
-t = (1:numSec*samplingRate);
 clear device;
+
+t = (1:numSec*samplingRate);
+data = horzcat(t',v);
 
 %Plot raw data
 % figure;
@@ -35,7 +26,5 @@ clear device;
 % title('Unprocessed Data');
 % xlabel('Time [ms]');
 % ylabel('Voltage [V]');
-
-data = horzcat(t',v);
 
 end
