37f254361d70fcb6708a15d855be844347db904c
Joanna Mialkowska <124645771+joannamialkowska@users.noreply.github.com>
2024-03-12 11:48:51 -0700

    readonly_1k.ino

--- readonly_1k.ino
+++ readonly_1k.ino
@@ -3,37 +3,26 @@
    unsigned t = 0;   // the time variable 
    unsigned v = 0;   // the voltage value
    unsigned s=0;  // start time
-  unsigned a = 0;
+   unsigned a = 0;
 
    const int analogInPin = A11;
-   
 
 //Set baud rate and input pin
    void setup() {
-   Serial.begin(115200); //try 230400
+   Serial.begin(115200);
    //Serial.begin(230400);
    pinMode(analogInPin, INPUT);
-   s=micros(); //systime = micros();
+   s=micros();
  }
 
 //Loop for reading samples of data
 void loop() {
-  t=micros()-s;    //micros()
-
+  t=micros()-s;
   
   if((int)(t - a) >= 0)  {
     v=analogRead(analogInPin);
-
-    //Serial.print(t); // time //ewentualnie zakomentowac
-    //Serial.print(' '); //ewentualnie zakomentowac
     Serial.println(v);  // voltage
     a += 1000;
   } 
-  //v=analogRead(analogInPin);
-
-  //Serial.print(t); // time
-  //Serial.print(' ');
-  //Serial.println(v);  // voltage
-  
-  delayMicroseconds(650);      // sampling interval in ms
+  delayMicroseconds(650);
 }
